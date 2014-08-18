dc2200d6a97e992a96f49ffcdb45a0b0f153620c
Vincent Villani <villanivincent@gmail.com>
2014-08-18 14:11:29 +1000

    Added wholeOuterProductSum, not yet tested

--- src/OuterProductFinal.cu
+++ src/OuterProductFinal.cu
@@ -10,20 +10,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static const int WORK_SIZE = 256;
+static const int BINSIZE = 1024;
 
+//Not a general outer product kernel, do not use in anything but DSPSR, due to implicit assumptions
 //Assumes lhsMatrixLength == rhsMatrixLength
-__global__ void outerProductSum(float* resultMatrix, float* lhsMatrix, float* rhsMatrix, unsigned int lhsMatrixLength)
+__global__ void wholeOuterProductSum(float* resultMatrix, float* lhsMatrix, float* rhsMatrix, unsigned int resultMatrixGridBlockRowIdx, unsigned int resultMatrixGridBlockColIdx)
 {
-	//Make each thread do more work if there is any more work to be done
-	for(int absoluteThreadIdx = blockDim.x * blockIdx.x + threadIdx.x; absoluteThreadIdx < lhsMatrixLength * lhsMatrixLength; absoluteThreadIdx += gridDim.x * blockDim.x)
-	{
-		//Write back to global memory
-		resultMatrix[absoluteThreadIdx] += lhsMatrix[ (int)floorf(absoluteThreadIdx / lhsMatrixLength) ] * rhsMatrix[absoluteThreadIdx % lhsMatrixLength];
-	}
+	//Absolute threadIdx within a block of the results grid
+	const int absoluteThreadIdx = blockDim.x * blockIdx.x + threadIdx.x;
+
+	//Calculate the index to write the result into the result matrix
+	const int indexRowCalc = absoluteThreadIdx / BINSIZE;
+	const int indexColCalc = absoluteThreadIdx % BINSIZE;
+	const int totalColumnSize = 4 * BINSIZE;
+
+	const int resultRow = (resultMatrixGridBlockRowIdx * BINSIZE) + indexRowCalc;
+	const int resultCol = (resultMatrixGridBlockColIdx * BINSIZE) + indexColCalc;
+	const int lowerTrianglarLength = (resultRow * (resultRow + 1)) / 2; //calculates the lowerTriangluarLength at this point
+
+	int resultMatrixIdx = (resultRow * totalColumnSize + resultCol) - lowerTrianglarLength;
+
+	//write output to the result matrix
+	resultMatrix[resultMatrixIdx] += lhsMatrix[indexRowCalc] * rhsMatrix[indexColCalc];
 }
+
+
+//Assumes square matrices
+__device__ __host__ unsigned int upperTriangularLength(unsigned int numRows)
+{
+	//printf("%d", (numRows * (numRows + 1)) / 2);
+	return (numRows * (numRows + 1)) / 2;
+}
+
 
 int main()
 {
+	upperTriangularLength(2);
 	return 0;
 }
